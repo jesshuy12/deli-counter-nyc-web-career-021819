d7a35731dcb3a58a2d75a8980156709c0a7ade20
Jessy Hu <jesshuy12@gmail.com>
2019-02-01 20:35:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,6 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.size == 0 
-    return "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
-    
+    puts 
