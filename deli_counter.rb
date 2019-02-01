b4d4fe252b041366cc459323ec984239c815ae8a
Jessy Hu <jesshuy12@gmail.com>
2019-02-01 20:28:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,9 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
+    phrase = "The line is currently:"
     katz_deli.each_with_index do |name, index|
-      puts "The line is currently: #{index+1}. #{name}"
+       #{index+1}. #{name}"
     end
 end
 end
