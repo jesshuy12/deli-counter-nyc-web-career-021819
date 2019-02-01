f3bd26d12fcb5176473d148d6e04fa01b271217e
Jessy Hu <jesshuy12@gmail.com>
2019-02-01 20:28:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
   else
     phrase = "The line is currently:"
     katz_deli.each_with_index do |name, index|
-       #{index+1}. #{name}"
+      phrase += "#{index+1}. #{name}"
     end
 end
 end
