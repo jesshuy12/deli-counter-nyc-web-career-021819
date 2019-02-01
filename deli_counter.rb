132fb055af128a13bed7442c171845a77376129d
Jessy Hu <jesshuy12@gmail.com>
2019-02-01 20:35:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,4 +19,8 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
+  if katz_deli.size == 0 
+    return "There is nobody waiting to be served!"
+  else
+    
